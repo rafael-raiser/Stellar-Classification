851a14d87341d77e072323ec92ce3402e1612838
Rafael Raiser | QSOFT <rafael.raiser@qsoft.com.br>
2024-02-15 11:36:55 -0300

    Data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from src import logger
+from src.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
-logger.info('Hello world!')
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
